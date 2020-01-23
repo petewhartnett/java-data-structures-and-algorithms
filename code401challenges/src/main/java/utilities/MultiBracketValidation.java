95bf202d15941dee6101a8fe5d96a763acb665e6
petewhartnett <petewhartnett@outlook.com>
2020-01-23 13:45:02 -0800

    solution is not working, going to refactor

---
+++ code401challenges/src/main/java/utilities/MultiBracketValidation.java
@@ -0,0 +1,18 @@
+package utilities;
+
+
+import linkedlist.LinkedList;
+import stacksandqueues.Stack;
+
+public class MultiBracketValidation {
+
+    String str = "{";
+    String str2 = "}";
+
+    Stack stack = new Stack();
+
+    stack.push(10);
+
+
+
+}
